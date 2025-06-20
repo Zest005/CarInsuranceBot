36d423446ac5535e3198375f75222c23a703302e
Daniil <Zest005@gmail.com>
2025-06-20 05:04:21 +0300

    Added receiving updates class

--- CarInsuranceBot.TelegramHost/HostedServices/TelegramPollingService.cs
+++ CarInsuranceBot.TelegramHost/HostedServices/TelegramPollingService.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
 
 namespace CarInsuranceBot.TelegramHost.HostedServices
 {
-    class TelegramPollingService
+    public class TelegramPollingService : BackgroundService
     {
+        private readonly ILogger<TelegramPollingService> _logger;
+        private readonly TelegramBotClient _botClient;
+        private readonly IUpdateHandler _updateHandler;
+
+        public TelegramPollingService(
+            ILogger<TelegramPollingService> logger,
+            TelegramBotClient botClient,
+            IUpdateHandler updateHandler)
+        {
+            _logger = logger;
+            _botClient = botClient;
+            _updateHandler = updateHandler;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+            };
+
+            _logger.LogInformation("Starting Telegram Bot...");
+
+            _botClient.StartReceiving(
+                updateHandler: _updateHandler,
+                receiverOptions: receiverOptions,
+                cancellationToken: stoppingToken);
+
+            var me = await _botClient.GetMe();
+            _logger.LogInformation("Bot started. Username: @{Username}", me.Username);
+        }
     }
 }
